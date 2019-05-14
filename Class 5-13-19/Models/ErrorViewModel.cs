e4880f2aa8b3184291e33529b81e29ba44ba3050
Tziri <tzirizohn1@gmail.com>
2019-05-13 21:12:22 -0400

    Add project files.

---
+++ Class 5-13-19/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Class_5_13_19.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
